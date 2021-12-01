354f6ec5cffbd50d5b60cd1fb412a2692b5cbdc1
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-01 14:27:09 -0300

    solution to day01 part2

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,4 @@
+# part 1
 def count_increases(depth_measurements):
     count = 0
     for i in range(1, len(depth_measurements)):
@@ -5,6 +6,22 @@ def count_increases(depth_measurements):
             count += 1
 
     return count
+
+
+# part 2
+def count_window_increases(depth_measurements, window_length=3):
+    count = 0
+    for i in range(window_length, len(depth_measurements)):
+        if sum_window(depth_measurements, i, window_length) > sum_window(
+            depth_measurements, i - 1, window_length
+        ):
+            count += 1
+
+    return count
+
+
+def sum_window(data, index, window_length):
+    return sum(data[index - window_length + 1 : index + 1])
 
 
 def read_file(filename):
@@ -19,9 +36,13 @@ def read_file(filename):
 
 
 def solve(filename):
-    return count_increases(read_file(filename))
+    input = read_file(filename)
+    part1_answer = count_increases(input)
+    part2_answer = count_window_increases(input)
+
+    return part1_answer, part2_answer
 
 
 if __name__ == "__main__":
-    answer = solve("input.txt")
-    print(answer)
+    part1_answer, part2_answer = solve("input.txt")
+    print(part1_answer, part2_answer)
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -1,7 +1,7 @@
 import pytest
 import main
 
-
+# part 1
 @pytest.mark.parametrize(
     "input, expected_increases_count",
     [
@@ -12,3 +12,29 @@ import main
 )
 def test_count_increases(input, expected_increases_count):
     assert main.count_increases(input) == expected_increases_count
+
+
+# part 2
+@pytest.mark.parametrize(
+    "input, expected_increases_count",
+    [
+        ((199, 200, 208, 210, 200, 207, 240, 269, 260, 263), 5),
+        ((0, 0, 6, 2), 1),
+        ((0, 0, 6, 2, 1, 1), 2),
+        ((1, 1, 1, 1, 1, 1), 0),
+    ],
+)
+def test_count_window_increases(input, expected_increases_count):
+    assert main.count_window_increases(input) == expected_increases_count
+
+
+@pytest.mark.parametrize(
+    "input, index, expected_sum",
+    [
+        ((199, 200, 208, 210, 200, 207, 240, 269, 260, 263), 2, 607),
+        ((199, 200, 208, 210, 200, 207, 240, 269, 260, 263), 3, 618),
+        ((199, 200, 208, 210, 200, 207, 240, 269, 260, 263), 9, 792),
+    ],
+)
+def test_sum_window(input, index, expected_sum):
+    assert main.sum_window(input, index, window_length=3) == expected_sum
