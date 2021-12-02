2713d7f2e7af59c51ae440c424595a33b3fef4c7
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-02 11:33:09 -0300

    day02 part2

--- day02/main.py
+++ day02/main.py
@@ -1,20 +1,38 @@
-def calculate_final_position(steps):
-    final_position = [0, 0]
-
-    for step in steps:
-        axis, value = get_step_axis(step)
-        final_position[axis] += value
-
-    return final_position, position_product(final_position)
-
-
-def get_step_axis(step):
+def step_parser_without_aim(step):
     command, value = step
     return {
         "forward": (0, value),
         "down": (1, value),
         "up": (1, -value),
     }[command]
+
+
+def step_parser_with_aim(step):
+    command, value = step
+    return {
+        "forward": (0, value),
+        "down": (2, value),
+        "up": (2, -value),
+    }[command]
+
+
+def calculate_final_position(
+    steps, step_parser=step_parser_without_aim, aim_multiplier=0
+):
+    final_position = [0, 0, 0]
+
+    for step in steps:
+        axis, value = step_parser(step)
+        final_position[axis] += value
+
+        if axis == 0:
+            final_position[1] += final_position[2] * value * aim_multiplier
+
+    return final_position, position_product(final_position)
+
+
+def position_product(position):
+    return position[0] * position[1]
 
 
 def read_file(filename):
@@ -29,17 +47,14 @@ def read_file(filename):
     return list(data)
 
 
-def position_product(position):
-    return position[0] * position[1]
-
-
 def solve(filename):
     input = read_file(filename)
-    part1_answer = calculate_final_position(input)
+    part1_answer = calculate_final_position(input, step_parser_without_aim, 0)
+    part2_answer = calculate_final_position(input, step_parser_with_aim, 1)
 
-    return part1_answer
+    return part1_answer, part2_answer
 
 
 if __name__ == "__main__":
-    part1_answer = solve("input.txt")
-    print(part1_answer)
+    part1_answer, part2_answer = solve("input.txt")
+    print(part1_answer, part2_answer)
