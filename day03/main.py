709130c33862d15f3dcd455023f23a96d1dcc26b
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-03 18:03:45 -0300

    day03 part2

--- day03/main.py
+++ day03/main.py
@@ -25,15 +25,55 @@ def calc_rates(zero_count, one_count):
     )
 
 
-def binary_to_decimal(binary):
-    return int(binary, 2)
-
-
 def calc_power_consuption(input):
     zero_count, one_count = count_bits(input)
     ((_, gamma_rate), (_, epsilon_rate)) = calc_rates(zero_count, one_count)
 
     return gamma_rate * epsilon_rate
+
+
+def filter_by_bit_count(input, index, reverse_criteria=False):
+    zero_count, one_count = count_bits(input)
+
+    criteria = "0" if zero_count[index] > one_count[index] else "1"
+
+    if reverse_criteria:
+        criteria = "1" if criteria == "0" else "0"
+
+    return [binary for binary in input if binary[index] == criteria]
+
+
+def calc_rating(input, reverse_criteria=False):
+    filtered_input = input
+    size = len(input[0])
+
+    for index in range(size):
+        filtered_input = filter_by_bit_count(filtered_input, index, reverse_criteria)
+        if len(filtered_input) == 1:
+            break
+
+    rating_binary = filtered_input[0]
+
+    return rating_binary, binary_to_decimal(rating_binary)
+
+
+def calc_o2_generator_rating(input):
+    return calc_rating(input, False)
+
+
+def calc_co2_scrubber_rating(input):
+    return calc_rating(input, True)
+
+
+def calc_life_support_rating(input):
+    (_, o2) = calc_o2_generator_rating(input)
+    (_, co2) = calc_co2_scrubber_rating(input)
+
+    return o2 * co2
+
+
+def binary_to_decimal(binary):
+    return int(binary, 2)
 
 
 def read_file(filename):
@@ -50,10 +90,12 @@ def read_file(filename):
 def solve(filename):
     input = read_file(filename)
     part1_answer = calc_power_consuption(input)
+    part2_answer = calc_life_support_rating(input)
 
-    return part1_answer
+    return part1_answer, part2_answer
 
 
 if __name__ == "__main__":
-    part1_answer = solve("input.txt")
-    print(part1_answer)
+    part1_answer, part2_answer = solve("input.txt")
+    print(f"part1: {part1_answer}")
+    print(f"part2: {part2_answer}")
--- day03/test_.py
+++ day03/test_.py
@@ -1,25 +1,33 @@
 import pytest
-from main import count_bits, calc_rates
+from main import (
+    count_bits,
+    calc_rates,
+    filter_by_bit_count,
+    calc_o2_generator_rating,
+    calc_co2_scrubber_rating,
+)
+
+TEST_INPUT = [
+    "00100",
+    "11110",
+    "10110",
+    "10111",
+    "10101",
+    "01111",
+    "00111",
+    "11100",
+    "10000",
+    "11001",
+    "00010",
+    "01010",
+]
 
 
 @pytest.mark.parametrize(
     "input, expected_zero_count, expected_one_count",
     [
         (
-            [
-                "00100",
-                "11110",
-                "10110",
-                "10111",
-                "10101",
-                "01111",
-                "00111",
-                "11100",
-                "10000",
-                "11001",
-                "00010",
-                "01010",
-            ],
+            TEST_INPUT,
             [5, 7, 4, 5, 7],
             [7, 5, 8, 7, 5],
         ),
@@ -49,3 +57,38 @@ def test_calc_rates(zero_count, one_count, expected_gamma_rate, expected_epsilon
         expected_gamma_rate,
         expected_epsilon_rate,
     )
+
+
+@pytest.mark.parametrize(
+    "input, index, expected",
+    [
+        (
+            ["001", "000", "100"],
+            0,
+            ["001", "000"],
+        ),
+        (
+            ["001", "000", "100"],
+            1,
+            ["001", "000", "100"],
+        ),
+        (
+            ["001", "001", "101", "000"],
+            2,
+            ["001", "001", "101"],
+        ),
+    ],
+)
+def test_filter_by_bit_count(input, index, expected):
+    assert filter_by_bit_count(input, index) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected_o2, expected_co2",
+    [
+        (TEST_INPUT, ("10111", 23), ("01010", 10)),
+    ],
+)
+def test_calc_gas_rating(input, expected_o2, expected_co2):
+    assert calc_o2_generator_rating(input) == expected_o2
+    assert calc_co2_scrubber_rating(input) == expected_co2
