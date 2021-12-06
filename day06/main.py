ea34718cd95add142d100cb67d3c054cd1349537
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-06 11:45:02 -0300

    day06 part2

--- day06/main.py
+++ day06/main.py
@@ -1,18 +1,24 @@
-def clock(state):
+def clock(state, max_age=8, respawn_age=6):
+    new_fish = state[0]
+
     def decrement_one(state):
-        return [timer - 1 for timer in state]
+        next_state = state
+        for i in range(max_age):
+            next_state[i] = state[i + 1]
 
-    def create_fish(state):
-        return [*state, *[8 for timer in state if timer == -1]]
+        return next_state
 
-    def normalize(state):
-        return [6 if timer == -1 else timer for timer in state]
+    def create_fish(state, new_fish):
+        new_state = state
+        new_state[respawn_age] += new_fish
+        new_state[max_age] = new_fish
+        return new_state
 
-    return normalize(create_fish(decrement_one(state)))
+    return create_fish(decrement_one(state), new_fish)
 
 
-def run_simulation(initial_state, days):
-    state = initial_state
+def run_simulation(input, days):
+    state = parse_to_state(input)
     for _ in range(days):
         state = clock(state)
 
@@ -20,7 +26,7 @@ def run_simulation(initial_state, days):
 
 
 def fish_count(state):
-    return len(state)
+    return sum(state)
 
 
 ###
@@ -33,12 +39,23 @@ def parse(filename):
     return list(input)
 
 
+def parse_to_state(input, max_age=8):
+    state = [0 for _ in range(max_age + 1)]
+    for i, _ in enumerate(state):
+        state[i] = len([timer for timer in input if timer == i])
+
+    return state
+
+
 def solve(filename):
     input = parse(filename)
     part1, _ = run_simulation(input, 80)
-    return part1
+    part2, _ = run_simulation(input, 256)
+
+    return part1, part2
 
 
 if __name__ == "__main__":
-    part1 = solve("input.txt")
+    part1, part2 = solve("input.txt")
     print(f"part1: {part1}")
+    print(f"part1: {part2}")
--- day06/test_.py
+++ day06/test_.py
@@ -2,23 +2,34 @@ import pytest
 from main import *
 
 
-@pytest.mark.parametrize(
-    "fish_list, next_day_fish_list",
-    [
-        ([3, 4, 3, 1, 2], [2, 3, 2, 0, 1]),
-        ([2, 3, 2, 0, 1], [1, 2, 1, 6, 0, 8]),
-        (
-            [0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 7, 8],
-            [6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 7, 8, 8, 8],
-        ),
-    ],
-)
-def test_clock(fish_list, next_day_fish_list):
-    assert clock(fish_list) == next_day_fish_list
-
-
 def test_part1():
     input = parse("example.txt")
-    # fmt: off
-    final_state = [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8] 
-    assert run_simulation(input, 18) == (26, final_state)
+    assert run_simulation(input, 18)[0] == 26
+
+
+def test_part2():
+    input = parse("example.txt")
+    assert run_simulation(input, 256)[0] == 26984457539
+
+
+@pytest.mark.parametrize(
+    "input, state",
+    [
+        ([3, 4, 3, 1, 2], [0, 1, 1, 2, 1, 0, 0, 0, 0]),
+        ([2, 3, 2, 0, 1], [1, 1, 2, 1, 0, 0, 0, 0, 0]),
+        ([1, 2, 1, 6, 0, 8], [1, 2, 1, 0, 0, 0, 1, 0, 1]),
+    ],
+)
+def test_parse_to_state(input, state):
+    assert parse_to_state(input) == state
+
+
+@pytest.mark.parametrize(
+    "state, next_state",
+    [
+        ([0, 1, 1, 2, 1, 0, 0, 0, 0], [1, 1, 2, 1, 0, 0, 0, 0, 0]),
+        ([1, 1, 2, 1, 0, 0, 0, 0, 0], [1, 2, 1, 0, 0, 0, 1, 0, 1]),
+    ],
+)
+def test_clock(state, next_state):
+    assert clock(state) == next_state
