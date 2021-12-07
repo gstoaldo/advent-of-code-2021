7e17184150e62a5960d3e50fd96cbfcb11adf7f3
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-07 07:32:39 -0300

    day07 part2

--- day07/main.py
+++ day07/main.py
@@ -1,19 +1,36 @@
-def total_fuel(crabs, position):
-    return sum(abs(crab - position) for crab in crabs)
+def constant_eq(position, position_to_move):
+    return abs(position - position_to_move)
 
 
-def find_best_position(crabs):
+def not_constant_eq(position, position_to_move):
+    return sum_pa(abs(position_to_move - position))
+
+
+def sum_pa(an):
+    a1 = 1
+    n = an
+    return (a1 + an) * n / 2
+
+
+def total_fuel(crabs, position, move_eq=constant_eq):
+    return sum(move_eq(crab, position) for crab in crabs)
+
+
+def find_best_position(crabs, move_eq=constant_eq):
     max_position = max(crabs)
-    best_fuel = total_fuel(crabs, 0)
+    best_fuel = total_fuel(crabs, 0, move_eq)
     best_position = 0
 
     for position in range(1, max_position + 1):
-        fuel = total_fuel(crabs, position)
+        fuel = total_fuel(crabs, position, move_eq)
         if fuel < best_fuel:
             best_fuel = fuel
             best_position = position
 
     return best_fuel, best_position
+
+
+###
 
 
 def parse(filename):
@@ -25,11 +42,13 @@ def parse(filename):
 
 def solve(filename):
     crabs = parse(filename)
-    part1 = find_best_position(crabs)
+    part1 = find_best_position(crabs, move_eq=constant_eq)
+    part2 = find_best_position(crabs, move_eq=not_constant_eq)
 
-    return part1
+    return part1, part2
 
 
 if __name__ == "__main__":
-    part1 = solve("input.txt")
+    part1, part2 = solve("input.txt")
     print(f"part1: {part1[0]}, position: {part1[1]}")
+    print(f"part2: {part2[0]}, position: {part2[1]}")
--- day07/test_.py
+++ day07/test_.py
@@ -17,3 +17,22 @@ def test_total_fuel(crabs, position, expected_fuel):
 def test_part1():
     input = parse("example.txt")
     assert find_best_position(input) == (37, 2)
+
+
+@pytest.mark.parametrize(
+    "position, position_to_move, expected_fuel",
+    [
+        (16, 5, 66),
+        (1, 5, 10),
+        (2, 5, 6),
+        (0, 5, 15),
+        (14, 5, 45),
+    ],
+)
+def test_not_constant_eq(position, position_to_move, expected_fuel):
+    assert not_constant_eq(position, position_to_move) == expected_fuel
+
+
+def test_part2():
+    input = parse("example.txt")
+    assert find_best_position(input, move_eq=not_constant_eq) == (168, 5)
