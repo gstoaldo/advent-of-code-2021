15a0fa07f8342941497cd869da225a4e2512d908
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2021-12-09 16:21:46 -0300

    day09 part2

--- day09/test_.py
+++ day09/test_.py
@@ -23,9 +23,9 @@ def test_is_local_min(input, i, j, expected_is_local_min):
 @pytest.mark.parametrize(
     "input, i, j, expected_adjacents",
     [
-        (SAMPLE, 0, 0, [9, 3]),
-        (SAMPLE, 0, 1, [2, 1, 2]),
-        (SAMPLE, 1, 1, [3, 3, 9, 9]),
+        (SAMPLE, 0, 0, [(0, 1), (1, 0)]),
+        (SAMPLE, 0, 1, [(0, 2), (0, 0), (1, 1)]),
+        (SAMPLE, 1, 1, [(1, 2), (1, 0), (0, 1), (2, 1)]),
     ],
 )
 def test_get_adjacents(input, i, j, expected_adjacents):
@@ -34,5 +34,24 @@ def test_get_adjacents(input, i, j, expected_adjacents):
 
 def test_part1():
     input = parse("example.txt")
-    assert get_local_mins(input) == [1, 0, 5, 5]
+    assert get_local_mins(input)[0] == [1, 0, 5, 5]
     assert risk_level_sum(input) == 15
+
+
+def test_get_basin():
+    basin = get_basin(
+        [
+            [1, 9, 4, 9],
+            [2, 4, 5, 6],
+            [9, 9, 4, 9],
+        ],
+        0,
+        0,
+    )
+    print(basin)
+    assert basin == [(0, 0), (1, 0), (1, 1), (1, 2), (1, 3), (0, 2), (2, 2)]
+
+
+def test_part2():
+    input = parse("example.txt")
+    assert get_largest_basins(input)[0] == 1134
